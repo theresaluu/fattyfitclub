0a47ccb5c8895f9655cc65a7599159f7a903b6df
Theresa Luu <theresa.luu@gmail.com>
2015-01-02 15:36:47 -0800

    Commented out RSPEC 4 stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,17 +26,17 @@ RSpec.configure do |config|
     #   # => "be bigger than 2 and smaller than 4"
     # ...rather than:
     #   # => "be bigger than 2"
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    # expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
-  config.mock_with :rspec do |mocks|
-    # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will default to
-    # `true` in RSpec 4.
-    mocks.verify_partial_doubles = true
-  end
+  # config.mock_with :rspec do |mocks|
+  #   # Prevents you from mocking or stubbing a method that does not exist on
+  #   # a real object. This is generally recommended, and will default to
+  #   # `true` in RSpec 4.
+  #   mocks.verify_partial_doubles = true
+  # end
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
