25924c574a103b9f33fff2f8c829dc0823c9bce6
Theresa Luu <theresa.luu@gmail.com>
2015-01-02 15:51:24 -0800

    changed focus of contest spec to weigh-ins instead of winning-stats

--- spec/models/contest_spec.rb
+++ spec/models/contest_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Contest, "#winning_stats" do
+describe Contest, "#weigh_ins" do
   let(:contest){
     Contest.create(
     name: "Cruise_Lose",
@@ -9,10 +9,15 @@ describe Contest, "#winning_stats" do
     end_date: (Time.now + 7776000.0)
     )}
 
+
   it "returns the amount of required weigh-ins" do
     duration = contest.end_date - contest.begin_date
     expect(contest.required_weigh_ins).to eq((duration / 7).floor)
     puts "weigh ins: " + contest.required_weigh_ins.to_s
   end
+
+  # it "returns a schedule of weigh-in deadlines" do
+  #   (contest.required_weigh_ins).times do
+  #     weigh_in_deadlines[ ] <<
 
 end
