30e9eff2a6804950998bc11773bf7ac86511ad5f
Theresa Luu <theresa.luu@gmail.com>
2015-01-02 16:58:14 -0800

    removed secret token off of gitignore- scary

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fattyfitclub::Application.config.secret_key_base = '9c69c4c028202b9c5172483c09683c4250a05858b1a80ba97a5bd1d4812c4ea2408731af14ad2bdd6cfc96b637791ff70c554e7337e01b6681ff502f8401d21d'
