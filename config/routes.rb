677cc01fe7f48de070569d24e39dac8a0fe8c300
Theresa Luu <theresa.luu@gmail.com>
2015-02-09 07:25:50 -0800

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Fattyfitclub::Application.routes.draw do
+  get "contests/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
